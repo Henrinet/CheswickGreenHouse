835e1012bb0541a08c2cd5c6d4b5a1cc2ed6198d
Henry <henrykan288@gmail.com>
2017-03-24 01:38:56 +0800

    23/03 stop

---
+++ includes/nav_old.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>	
+        </button>
+	<a class="navbar-brand" href="index.php">Cheswick Green House</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="about.php">About us</a></li>
+        <li><a href="product.php">Product</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="news.php">News</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign in</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -2,172 +2,30 @@
 <?php include "includes/overall/header.php"; ?>
 
 <section class="main-content" >
-      <link rel="preload" href="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/index.scss.css?294678486489210881" as="style" onload="this.rel='stylesheet'">
-<noscript><link rel="stylesheet" href="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/index.scss.css?294678486489210881"></noscript>
-<div class="container-fluid">
-  <div class="row-section" id="top-row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_01_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_01_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <h2 class="section-title">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_01_fg.png?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_01_fg_250x.png?294678486489210881" alt="" style="display: none">
-      </h2>
-    </div>
-    <div class="section-description-wrap description-wide">
-      <p>Food is a remarkable thing – it brings families together, supports communities and gives farmers a means. It nourishes our bodies and brings joy to our tastebuds.</p>
-      <a class="btn btn-description-action" href=""></a>
-    </div>
-  </div>
-</div>
-<div class="container-fluid container-alt container-left">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_02_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_02_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <h2 class="section-title">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_02_fg.png?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_02_fg_250x.png?294678486489210881" alt="" style="display: none">
-      </h2>
-    </div>
-    <div class="section-description-wrap">
-      <p>We stand for good food and the good things it does for people &ndash; for you, for the farmer, for our kids&hellip; and their kids too. We don&rsquo;t just bring you amazing foods, we do it at great prices. And then we sprinkle it with the goodness of initiatives like &lsquo;Imperfect Picks&rsquo; and &lsquo;Love Local&rsquo;.  We call that <u>value</u> with <u>values</u>.</p>
-      <a class="btn btn-description-action" href="/blogs/campaigns">Interested in Values?</a>
-    </div>
-  </div>
-</div>
-<div class="container-fluid container-alt">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_03_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_03_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <div class="homepage-animation-bee">
-          <img src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/homepage-anim-bee-03.gif?294678486489210881" alt="" style="left: -10%;">
-      </div>
-      <h2 class="section-title">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_03_fg.png?294678486489210881" alt="" style="left: -20%; width: 55%;">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_03_fg_250x.png?294678486489210881" alt="" style="left: -20%; width: 55%; display: none;">
-      </h2>
-    </div>
-    <div class="section-description-wrap description-wide">
-      <p>We marvel at the amazing, unpredictable, magical thing that is nature. Its riches, surprises and struggles. We don&rsquo;t just respect nature &ndash; we&rsquo;re driven by it. Always have been and always will be. When you understand what nature is doing, you&rsquo;ll understand a lot more about what you find in our stores.</p>
-      <a class="btn btn-description-action btn-yellow" href="/blogs/daves-market-update">What's nature been up to?</a>
-    </div>
-  </div>
-</div>
-<div class="container-fluid">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_04_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_04_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <h2 class="section-title">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_04_fg.png?294678486489210881" alt="" style="left: -40%; width: 80%;">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_04_fg_250x.png?294678486489210881" alt="" style="left: -40%; width: 80%; display: none;">
-      </h2>
-    </div>
-    <div class="section-description-wrap description-wide">
-      <p style="">When you&rsquo;re driven by nature you don&rsquo;t try to tell her what to do. We set our specials only when we find out what nature&rsquo;s decided to give us. If she sees fit to bulge our baskets with blueberries, we think that&rsquo;s pretty special&hellip; so we put them on special.</p>
-      <a class="btn btn-description-action btn-gray" href="/pages/our-stores">On special this week</a>
-    </div>
-  </div>
-</div>
-<div class="container-fluid container-alt">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_05_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_05_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <h2 class="section-title">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_05_fg.png?294678486489210881" alt="" style="left: -50%; width: 100%;">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_05_fg_500x.png?294678486489210881" alt="" style="left: -50%; width: 100%; display: none;">
-      </h2>
-    </div>
-    <div class="section-description-wrap description-wide">
-      <p style="">Natural food is one of life&rsquo;s true joys &ndash; and shopping for it should be too! We deliver fresh fruit and veg from the markets every day and hunt down specialty products from artisan producers all over the country. That way you can ditch the list and shop with your senses. Smell the herbs. Marvel at the colours. Discover a little gem &ndash; go on enjoy it!</p>
-      <a class="btn btn-description-action" href=""></a>
-    </div>
-  </div>
-</div>
-<div class="container-fluid container-alt">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_06_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_06_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-    </div>
-  </div>
-</div>
-<div class="container-fluid container-alt">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_07_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_07_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <h2 class="section-title">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_07_fg.png?294678486489210881" alt="" style="left: -25%; width: 50%;">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_07_fg_500x.png?294678486489210881" alt="" style="left: -25%; width: 50%; display: none;">
-      </h2>
-    </div>
-    <div class="section-description-wrap description-wide">
-      <p style="">We&rsquo;re a team of over 1500 Aussies. We&rsquo;ve been family owned since 1971 when our founder David Harris  opened a single fruit shop in Villawood in Sydney&rsquo;s west. Our family values are still the values that drive us today.</p>
-    </div>
-  </div>
-</div>
-<div class="container-fluid">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_08_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_08_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <h2 class="section-title">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_08_fg.png?294678486489210881" alt="" style="left: -25%; width: 50%;">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_08_fg_500x.png?294678486489210881" alt="" style="left: -25%; width: 50%; display: none;">
-      </h2>
-    </div>
-    <div class="section-description-wrap description-wide">
-      <p style="">Oops, you forgot. You jumped online to get ideas for dinner. Then somehow you ended up here, (and we&rsquo;re really glad you did). Sooo, dinner. How about something like this...</p>
-    </div>
-  </div>
-</div>
-<div class="container-fluid container-hero">
-  <div class="row-section">
-    <div class="section-title-wrap">
-      <div class="section-background">
-        <img class="desktoponly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_09_bg_1280x.jpg?294678486489210881" alt="">
-        <img class="mobileonly" src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_09_bg_500x.jpg?294678486489210881" alt="" style="display: none">
-      </div>
-      <h2 class="section-title">
-        <a href="/collections/wholewheat-tomato-ricotta-galette">
-          <img src="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/home_block_09_fg.png?294678486489210881" alt="" style="left: -50%; width: 100%;">
-        </a>
-      </h2>
-    </div>
-  </div>
-</div>
-<div class="container-fluid container-cta">
-  <div class="row-section">
-    <div class="section-cta">
-      <a class="btn btn-action btn-center" href="/product.php">See our product!</a>
-    </div>
-  </div>
-</div>
+    <link rel="preload" href="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/index.scss.css?294678486489210881" as="style" onload="this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="//cdn.shopify.com/s/files/1/0206/9470/t/200/assets/index.scss.css?294678486489210881"></noscript>
 
+    <div class="section-background">
+          <a href="/product.php">
+            <img class="desktoponly" src="/image/home/home_block_03_bg_1280x.jpg" alt="">
+            <img class="mobileonly" src="/image/home/home_block_03_bg_1280x.jpg" alt="" style="display: none">
+          </a>
+    </div>
 
+    <div class="section-background">
+          <a href="/product.php">
+            <img class="desktoponly" src="/image/home/home_block_01_bg_1280x.jpg" alt="">
+            <img class="mobileonly" src="/image/home/home_block_01_bg_1280x.jpg" alt="" style="display: none">
+          </a>
+    </div>
 
-  </section>
-
+    <div class="section-background">
+          <a href="/product.php">
+            <img class="desktoponly" src="/image/home/home_block_02_bg_1280x.jpg" alt="">
+            <img class="mobileonly" src="/image/home/home_block_02_bg_1280x.jpg" alt="" style="display: none">
+          </a>
+    </div>
+</section>
 
 <?php include "includes/overall/footer.php"; ?>
 
